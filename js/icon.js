d95f86b229fc019607732561e6778f01e963f79d
苗桐菲 <q826761263@163.com>
2023-12-12 12:42:26 +0800

    完成部分搜索界面

---
+++ js/icon.js
@@ -0,0 +1,38 @@
+var jarr=[{
+    link:"#",
+    icon:"#icon-shouye",
+    txt:"首页"
+},{
+    link:"youxuan.html",
+    icon:"#icon-youxuan",
+    txt:"优选"
+},{
+    link:"#",
+    icon:"#icon-shipin",
+    txt:"视频"
+},{
+    link:"xiaoxi.html",
+    icon:"#icon-xiaoxi",
+    txt:"消息"
+},{
+    link:"wode.html",
+    icon:"#icon-wode",
+    txt:"我的"
+}];
+// dom:
+var jlist=document.querySelector(".xia");
+
+jarr.forEach((v,i)=>{
+    var nli=document.createElement("li");
+    nli.innerHTML=` <a href="${v.link}">
+    <div>
+    <svg class="icon" aria-hidden="true">
+    <use xlink:href="${v.icon}"></use>
+    </svg>
+    </div>
+    <p>${v.txt}</p>
+    </a>`;
+    //反引号
+    jlist.appendChild(nli);
+    //增加到内部的最后
+})
