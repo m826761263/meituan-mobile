be951bd644ad9ab3d4a87cb1ef287736b727fc7b
苗桐菲 <q826761263@163.com>
2023-12-12 22:30:22 +0800

    将index的css分成common.css和index.css两部分。完成部分位置界面

--- js/swiper.js
+++
@@ -1,9 +0,0 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    freeMode: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
